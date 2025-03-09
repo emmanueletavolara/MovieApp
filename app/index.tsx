ea8d41b55f340fe47509bb8305bfadc4d221137f
unknown <emmanueletavolara@gmail.com>
2025-03-09 16:03:14 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className= "text-5xl text-accent"> WELCOME TO THE  </Text>
+      <Text className= "text-5xl text-secondary ">  Welcome! to the s </Text>
+      <Text className= "text-5xl text-secondary"> REACT NATIVE </Text>
+      <Text className= "text-5xl text-primary"> STARTER KIT </Text>
+      <Text className= "text-5xl text-blue-500"> WELCOME TO THE  </Text>
+      <Text className= "text-5xl text-dark-100"> WELCOME TO THE  </Text>
+      <Text className= "text-5xl text-light-200 ">  Welcome! to the s </Text>
+      <Text className= "text-5xl text-secondary"> REACT NATIVE </Text>
+      <Text className= "text-5xl text-primary"> STARTER KIT </Text>
+      <Text className= "text-5xl text-yellow-800"> WELCOME TO THE  </Text>
+
+
+    </View>
+  );
+}
